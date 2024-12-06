017e5fc43eeb76bac561e6165d4c2be52ba7fa03
Raghav-558 <jangraraghav9211@gmail.com>
2024-12-06 12:34:21 +0530

    File setup

---
+++ src/components/hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hero = () => {
+    return (
+        <div className='hero-bg bg-center bg-cover '>
+            <div className='max-w-[1140px] mx-auto px-4'>
+
+            </div>
+        </div>
+    )
+}
+
+export default Hero
---
+++ src/utils/Helper.js
@@ -0,0 +1,18 @@
+export const ACCORDION_DATA = [
+    {
+        Title: 'Who is eligible for a Growth Plan? ',
+        Description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus eligendi autem recusandae in, corrupti eum aliquid molestiae facere accusantium deleniti?'
+    },
+    {
+        Title: 'Who is eligible for a Growth Plan? ',
+        Description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus eligendi autem recusandae in, corrupti eum aliquid molestiae facere accusantium deleniti?'
+    },
+    {
+        Title: 'Who is eligible for a Growth Plan? ',
+        Description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus eligendi autem recusandae in, corrupti eum aliquid molestiae facere accusantium deleniti?'
+    },
+    {
+        Title: 'Who is eligible for a Growth Plan? ',
+        Description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus eligendi autem recusandae in, corrupti eum aliquid molestiae facere accusantium deleniti?'
+    }
+]
